fe6969429bebd62c91daf765e1117d82eae7391e
CarlosEA2909 <ceinnova00088@innovaccion.mx>
2022-06-03 17:03:05 -0500

    initial commit

---
+++ Condicionales.py
@@ -0,0 +1,25 @@
+#Declaramos un variable
+calificacion = input("Introduce la calificacion de AZ-900: ")
+
+calificacion = int(calificacion)
+
+#Preguntamos si la calificacion es menor a 700
+if calificacion < 700:
+    print("Vessss, por no estudiar burrrooooooo") #Si es menor a 700, muestro esto
+elif calificacion > 1000:
+    print("Mientes!!, No puedes sacar mas de 1000")
+else : # Si no se cumple el if anterior pasa a esta linea
+    print("Felicidades, pasa por tu certificado") #Se ejecuta si ninungo de los if se cumple
+    
+#Verificador de mayor de edad    
+edad = input("Introduce tu edad: ")
+edad= int(edad)
+
+if edad >= 18 and edad<= 100 :
+    print("Bienvenido al mamitas")
+elif edad  > 100 :
+    print("Si vienes acompañado de tus abuelitos, si te podemos fiar ")
+elif edad < 0 :
+    print("Ni que fueras viajero del tiempo")
+else: 
+    print("No puedes llevarte esos cigarros")
---
+++ Tipos variables.py
@@ -0,0 +1,28 @@
+#String - es una cadena de texto
+from cmath import pi
+
+
+nombre = "Carlos👀"
+
+#Enteros - Numeros sin punto decimal
+edad = 19
+
+edads = "19"
+#Ejemplo de diferencia
+print(edad + edad)
+print(edads + edads )
+
+#flotante - numero con punto decimal
+pi = 3.1416
+
+edad = float(edads)#Casteo - transformar un tipo de variable a otro
+
+#Usar Type para saber de que tipo es la varibale
+print(type(nombre), type(edad))
+
+#Bool - Booleano- SI o NO
+legustas = False
+
+legustas = True
+
+print(legustas)
